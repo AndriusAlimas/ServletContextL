ab4108a0a5c42565e37d013b1afd58d1be80608c
Andrius <andriusjavait@gmail.com>
2017-01-16 16:58:05 +0000

    fully redone web.xml plus listener class initialization done

--- ServletContextL/src/com/example/listener/MyServletContextListener.java
+++ ServletContextL/src/com/example/listener/MyServletContextListener.java
@@ -1,17 +1,29 @@
 package com.example.listener;
 import javax.servlet.*;
-
 public class MyServletContextListener implements ServletContextListener {
-
+	private String className;
+	
+	// Constructor, all it doeas just get class name and store to String variable
+	public MyServletContextListener() {
+        className = this.getClass().getName();
+    }
+	
 	@Override
-	public void contextDestroyed(ServletContextEvent arg0) {
+	public void contextDestroyed(ServletContextEvent event) {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void contextInitialized(ServletContextEvent arg0) {
-		// TODO Auto-generated method stub
+	public void contextInitialized(ServletContextEvent event) {
+		 System.out.println("\n*****************************************\n");
+	     System.out.println("ContextInitialized method was called from " + className);
+	     System.out.println("Getting ServletContext object from event!");
+	     ServletContext sc = event.getServletContext();
+	     System.out.println(sc);
+	     System.out.println("Now getting from context parameter with value:");
+	     String babyName = sc.getInitParameter("BabyName");
+	     System.out.println(babyName);
 		
 	}
 
