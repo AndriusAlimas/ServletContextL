c1897af113d198c9f763a73f88971a3bce8c36f9
Andrius <andriusjavait@gmail.com>
2017-01-16 15:14:56 +0000

    added index.html with full functionality added MyServlet template code added MyServletContextListener template code full update DD with servlet, mapping and listener

---
+++ ServletContextL/src/com/example/listener/MyServletContextListener.java
@@ -0,0 +1,18 @@
+package com.example.listener;
+import javax.servlet.*;
+
+public class MyServletContextListener implements ServletContextListener {
+
+	@Override
+	public void contextDestroyed(ServletContextEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
