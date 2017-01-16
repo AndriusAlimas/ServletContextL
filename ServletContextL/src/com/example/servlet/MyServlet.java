4b01a6cd27fb4814680101db1a79df6275a06ec8
Andrius <andriusjavait@gmail.com>
2017-01-16 15:41:00 +0000

    update MyServlet class

--- ServletContextL/src/com/example/servlet/MyServlet.java
+++ ServletContextL/src/com/example/servlet/MyServlet.java
@@ -6,6 +6,13 @@ import java.io.*;
 public class MyServlet extends HttpServlet {
 	public void doGet(HttpServletRequest request,
 			HttpServletResponse response)throws ServletException,IOException{
-		
+			response.setContentType("text/html");
+			
+	        PrintWriter out = response.getWriter();
+	        out.println("<html>");
+	        out.println("<head><title>Test ServletContextListener </title></head>");
+	        out.println("<hr>Server Version: <i><b>"+getServletContext().getMajorVersion() + "</i></b>");
+	        out.println("<hr>Server Info: <i><b>"+getServletContext().getServerInfo() + "</i></b>");
+	        out.println("</body></html>");
 	}
 }
